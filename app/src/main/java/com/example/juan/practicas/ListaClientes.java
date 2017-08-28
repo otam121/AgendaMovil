6cb5091a6176ee2eaec8f5cec9d31f18253119f1
otam121 <santos_0019@hotmail.com>
2017-08-27 21:35:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/juan/practicas/ListaClientes.java
@@ -0,0 +1,47 @@
+package com.example.juan.practicas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import objetos.FirebaseReferenses;
+
+public class ListaClientes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_clientes);
+
+        //Toast de la clase
+        Toast toastpincorrecto = Toast.makeText(getApplicationContext(),"Pin Correcto sesion inciada",Toast.LENGTH_SHORT);
+
+        //Mensaje de inicio de sesion correcta
+        toastpincorrecto.show();
+
+        //Conectando con nuestra base de datos Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //haciendo referencia a los datos que queremos
+        DatabaseReference refusuarios = database.getReference(FirebaseReferenses.CLIENTES_REFERENSES);
+        refusuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String valor = dataSnapshot.getValue().toString();
+                Log.i("DATOS",valor);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("ERROR",databaseError.getMessage());
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/objetos/FirebaseReferenses.java
@@ -0,0 +1,9 @@
+package objetos;
+
+/**
+ * Created by Juan on 26/08/2017.
+ */
+
+public class FirebaseReferenses {
+    final public static String CLIENTES_REFERENSES = "clientes";
+}
